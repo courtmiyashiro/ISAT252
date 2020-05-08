7fad8963e27b02eaa9468632304fb66182de13dc
Courtney Miyashiro <59979452+c0deney@users.noreply.github.com>
2020-05-07 21:34:19 -0400

    Add files via upload

---
+++ Python/FizzBuzz.py
@@ -0,0 +1,45 @@
+#fizzbuzz1
+
+x = 0
+
+for x in range(1,101):
+  if (x % 3 == 0) and (x % 5 == 0):
+    print("Fizzbuzz")
+  elif x % 3 == 0:
+    print("Fizz")
+  elif x % 5 == 0:
+    print("Buzz")
+  else:
+    print(x)
+    
+"""A FizzBuzz program"""
+
+# import necessary supporting libraries or packages
+from numbers import Number
+
+def fizz(x):
+  """
+    Takes an input `x` and checks to see if x is a
+    number, and if so, also a multiple of 3.
+    If it is both, return 'fizz'.
+    Otherwise, return the input.
+  """
+  return 'fizz' if isinstance(x, Number) and x % 3 == 0 else x
+
+def buzz(x):
+  """
+    Takes an input `x` and checks to see if x is a
+    number, and if so, also a multiple of 5.
+    If it is both, return 'buzz'.
+    Otherwise, return the input.
+  """
+  return 'buzz' if isinstance(x, Number) and x % 5 == 0 else x
+
+def fibu(x):
+  """
+    Takes an input `x` and checks to see if x is a
+    number, and if so, also a multiple of 15.
+    If it is both, return 'fizzbuzz'.
+    Otherwise, return the input.
+  """
+  return 'fizzbuzz' if isinstance(x, Number) and x % 15 == 0 else x
